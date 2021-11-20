d6ea843eacb943c50a5edde0c873fec075c2e5e2
debasish2110 <debasishdash98@gmail.com>
2021-11-20 12:40:01 +0530

    models added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.charField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    def approve_comment(self):
+        return self.comments.fileter(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk': self.pk})   # pk -> primarykey
+
+    def __str__(self):
+        return self.title
+
+class comments(models.Model):
+    post = models.models.ForeignKey('blog.Post',related_name = 'comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comments = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comments = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('pot_list')
+
+    def __str__(self):
+        return self.text
